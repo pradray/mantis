5cd4da6017144fc4d7da89c95ce614df0fd004c4
pradray <pradray@users.noreply.github.com>
2018-05-31 14:28:19 +0530

    Added files
    Added springboot infra

---
+++ mantis-app/src/main/java/com/mantis/mantisapp/MantisAppApplication.java
@@ -0,0 +1,12 @@
+package com.mantis.mantisapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantisAppApplication.class, args);
+	}
+}
